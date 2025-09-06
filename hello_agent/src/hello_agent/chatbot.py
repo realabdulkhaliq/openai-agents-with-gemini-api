a638f144211fa6d9dd034e72276814a0ece55c74
ABDUL KHALIQ <thenewagedevelopers@gmail.com>
2025-09-06 14:09:06 +0500

    Ignore chainlit error - all is working

--- hello_agent/src/hello_agent/chatbot.py
+++ hello_agent/src/hello_agent/chatbot.py
@@ -2,4 +2,4 @@ import chainlit as cl
 
 @cl.on_message
 async def main(message: cl.Message):
-    await cl.Message(content=f"Received from AK: {message.content}!").send()
+    await cl.Message(content=f"Received: {message.content}!").send()
